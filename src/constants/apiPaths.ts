566c334d4f3e542015704edb81de1bb48e9f93c4
Ivan Shastakou <ivan_shastakou2@epam.com>
2023-02-25 15:29:50 +0100

    TASK-3: Changed url to fetch list of products

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  products: "https://2wekguza04.execute-api.eu-west-1.amazonaws.com/products",
+  order: "",
+  import: "",
+  bff: "",
+  cart: "",
 };
 
 export default API_PATHS;
