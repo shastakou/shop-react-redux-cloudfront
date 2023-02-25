3c068625b498fd127d9630321647763c1a52b092
Ivan Shastakou <ivan_shastakou2@epam.com>
2023-02-25 15:29:50 +0100

    task-3: Changed url to fetch list of products

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,10 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  products:
+    "https://1370boh5f1.execute-api.eu-west-1.amazonaws.com/dev/products",
+  order: "",
+  import: "",
+  bff: "",
+  cart: "",
 };
 
 export default API_PATHS;
--- src/queries/products.ts
+++ src/queries/products.ts
@@ -8,9 +8,7 @@ export function useAvailableProducts() {
   return useQuery<AvailableProduct[], AxiosError>(
     "available-products",
     async () => {
-      const res = await axios.get<AvailableProduct[]>(
-        `${API_PATHS.bff}/product/available`
-      );
+      const res = await axios.get<AvailableProduct[]>(`${API_PATHS.products}`);
       return res.data;
     }
   );
